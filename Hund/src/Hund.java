e9df08ca848ea987967ad77ba5911a3ab61d5933
MarekCzernohous <MarekCzernohous@ALZi-Marek>
2021-10-27 10:10:25 +0200

    Aufgabenimplementation

--- Hund/src/Hund.java
+++ Hund/src/Hund.java
@@ -1,4 +1,30 @@
 
 public class Hund {
+	String rasse;
 
+	String name;
+
+	int alter; 
+
+	double lautstaerkeDB; 
+	
+	public Hund(String name, int alter, double lautstaerkeDB) {
+		this.name = name;
+		this.alter=alter;
+		this.lautstaerkeDB = lautstaerkeDB;
+	}
+	
+	public Hund(String name) {
+		this(name, 10, 5);
+	}
+	
+	public Hund() {
+		this("Hundi");
+	}
+	
+	public double getLautstaerkeDB() {
+		return lautstaerkeDB;
+	}
+	
+	
 }
--- Hund/src/Schaeferhund.java
+++ Hund/src/Schaeferhund.java
@@ -1,3 +1,12 @@
 public class Schaeferhund extends Hund{
 
+	public Schaeferhund(String name, int alter, double lautstaerkeDB) {
+		super(name, alter, lautstaerkeDB);
+		this.rasse="Schaeferhund";
+		
+	}
+	
+	@Override public double getLautstaerkeDB(){
+		return 1.5*super.getLautstaerkeDB();
+	}
 }
