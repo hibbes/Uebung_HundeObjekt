60f36cc742d1a1dc750e4571f0602feb5ca9a459
hibbes <marek@czernohous.de>
2021-10-27 10:10:25 +0200

    Aufgabenimplementation

--- Hund/src/Main.java
+++ Hund/src/Main.java
@@ -2,8 +2,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Hund h1 = new Hund("Thorsten",7,31.4);
+		
+		System.out.println(h1.getLautstaerkeDB());
 
+		// Weitere Testroutinen
+		
+		Schaeferhund s1 = new Schaeferhund("Blutbert", 6, 22.6);
+		System.out.println(s1.rasse);
+		System.out.println(s1.getLautstaerkeDB());
+		
 	}
 
 }
